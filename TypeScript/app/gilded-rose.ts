72782e627da470c358ef429751e3ecb8802d8442
joel bongima <joelbong@hotmail.com>
2024-10-26 21:15:33 +0200

    refactor(gildedrose): :white_check_mark: item quality cannot be bigger than 50

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,52 +1,64 @@
-export class Item {
-  name: string;
-  sellIn: number;
-  quality: number;
-
-  constructor(name, sellIn, quality) {
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
-  }
-}
-
-export class ItemQuality {
-  static validateQuality(item: Item) {
-    if (item.quality < 0) {
-      throw new QualityNegativeError();
-    }
-    return item;
-  }
-}
-
 export class GildedRose {
   items: Array<Item>;
+  private itemUpdater: ItemUpdater = new ItemUpdater();
 
   constructor(items = [] as Array<Item>) {
-    this.items = items.map((item) => ItemQuality.validateQuality(item));
+    this.items = items.map((item) =>
+      this.itemUpdater.validateQualityValue(item)
+    );
   }
 
   updateQuality() {
     this.items = this.items.map((item) => {
       return item;
     });
-  }
-
-  getItems() {
     return this.items;
   }
 }
 
+export class Item {
+  name: string;
+  sellIn: number;
+  quality: number;
+
+  constructor(name: string, sellIn: number, quality: number) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
+}
+
+export class ItemUpdater {
+  validateQualityValue(item: Item) {
+    if (item.quality < 0) {
+      throw new QualityNegativeError(item);
+    }
+    if (item.name !== "Sulfuras, Hand of Ragnaros" || item.quality > 50) {
+      throw new QualityValueExceeded(item, 50);
+    }
+    return item;
+  }
+}
+
 class QualityValidationError extends Error {
-  constructor(message: string) {
-    super(message);
+  constructor(item: Item, errorMessage: string) {
+    super(
+      `${errorMessage} for item ${item.name} with quality ${item.quality} and sellIn ${item.sellIn}`
+    );
     this.name = "QualityValidationError";
   }
 }
 
 export class QualityNegativeError extends QualityValidationError {
-  constructor() {
-    super("Quality cannot be less than 0");
-    this.name = "QualityValidationError";
+  constructor(item: Item) {
+    super(item, "Quality cannot be less than 0");
+    this.name = "QualityNegativeError";
+  }
+}
+
+export class QualityValueExceeded extends QualityValidationError {
+  constructor(item: Item, expectedValue: number) {
+    super(item, `Quality cannot be bigger than ${expectedValue}`);
+    this.name = "QualityValueExceeded";
   }
 }
--- TypeScript/test/vitest/gilded-rose.spec.ts
+++ TypeScript/test/vitest/gilded-rose.spec.ts
@@ -1,7 +1,19 @@
-import { Item, GildedRose, QualityNegativeError } from '@/gilded-rose';
+import {
+  GildedRose,
+  Item,
+  QualityNegativeError,
+  QualityValueExceeded,
+} from "@/gilded-rose";
 
-describe('Gilded Rose', () => {
-  it('New item quality cannot be zero', () => {
-    expect(() => new GildedRose([new Item('foo', 0, -1)])).toThrow(QualityNegativeError)
+describe("Gilded Rose", () => {
+  it("New item quality cannot be zero", () => {
+    expect(() => new GildedRose([new Item("foo", 0, -1)])).toThrow(
+      QualityNegativeError
+    );
+  });
+  it("New item quality cannot be bigger than 50", () => {
+    expect(() => new GildedRose([new Item("foo", 0, 52)])).toThrow(
+      QualityValueExceeded
+    );
   });
 });
